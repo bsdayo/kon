d4d0421d86358ef0b5c5279984bbe8e56557b31d
bsdayo <bsdayo@outlook.com>
2024-06-24 16:46:27 +0800

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Mime;
+using System.Text.Json;
+using Tomlyn.Extensions.Configuration;
+
+var app = WebApplication.Create(args);
+
+var config = new ConfigurationBuilder();
+foreach (var file in new DirectoryInfo(app.Environment.ContentRootPath).GetFiles())
+{
+    var loaded = false;
+    switch (file.Extension)
+    {
+        case ".json":
+            config.AddJsonFile(file.FullName, optional: true, reloadOnChange: true);
+            loaded = true;
+            break;
+
+        case ".toml":
+            config.AddTomlFile(file.FullName, optional: true, reloadOnChange: true);
+            loaded = true;
+            break;
+
+        case ".ini":
+            config.AddIniFile(file.FullName, optional: true, reloadOnChange: true);
+            loaded = true;
+            break;
+    }
+
+    if (loaded)
+        app.Logger.LogInformation("Loaded {Extension} configuration file from {Path}.", file.Extension, file.FullName);
+}
+
+var root = config.Build();
+var token = app.Configuration["Token"];
+
+if (token is null)
+    app.Logger.LogWarning("Token not set! Anyone can access the entire configuration without any authentication.");
+
+app.MapGet("/", async context =>
+{
+    if (token is not null && context.Request.Headers.Authorization != $"Bearer {token}")
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return;
+    }
+
+    context.Response.ContentType = MediaTypeNames.Application.Json;
+    await using var writer = new Utf8JsonWriter(context.Response.Body);
+    writer.WriteStartObject();
+    foreach (var (key, value) in root.AsEnumerable())
+        if (value is not null)
+            writer.WriteString(key, value);
+    writer.WriteEndObject();
+});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,64 @@
+﻿# Kon
+
+A ultra simple "centralized configuration server" for distributed systems using .NET ecosystem.
+
+Scans configuration files in directory and serve them as a single flattened JSON object.
+
+Currently, supports `.json`, `.toml`, and `.ini` files.
+
+## Getting started
+
+### Basic usage
+
+Prepare some configuration files in a directory, like, hello.json:
+
+```json
+{
+  "Hello": "World!",
+  "ConnectionStrings": {
+    "MyDatabase": "Some=Thing; Here=There;"
+  }
+}
+```
+
+Serve all configuration files in current working directory:
+
+```shell
+$ kon
+```
+
+Curl it and check the response:
+
+```shell
+$ curl -s localhost:5000 | jq
+{
+  "Hello": "World!",
+  "ConnectionStrings:MyDatabase": "Some=Thing; Here=There;"
+}
+```
+
+### Authentication
+
+Add `--token` option (or set the `TOKEN` environment variable) to enable authentication:
+
+```shell
+$ kon --token myauthtoken
+```
+
+Then provide the token in `Authorization` header:
+
+```shell
+$ curl -s localhost:5000 -H "Authorization: Bearer myauthtoken" | jq
+```
+
+### Change listening port
+
+Specify listener prefix:
+
+```shell
+$ kon --urls http://127.0.0.1:80
+```
+
+## License
+
+[MIT](LICENSE)
